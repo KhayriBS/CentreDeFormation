dbd6da7a426d5ea4a2741776c10f16cb412cc921
BEN SALAH Khaireddine <khiareddine.bensalah@esprit.tn>
2024-12-08 08:51:52 -1200

    ajout dependance h2DataBase

--- Backend/src/main/java/com/formation/projet/repository/UserRepository.java
+++ Backend/src/main/java/com/formation/projet/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Integer>{
 	public Optional<User> findByUserName(String userName);
 	public boolean existsByEmail(String email);
