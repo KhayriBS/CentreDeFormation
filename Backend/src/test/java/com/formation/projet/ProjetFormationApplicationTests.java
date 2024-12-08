e49dfa89e0f58e21b613addcd4b67a67a0913ba8
BEN SALAH Khaireddine <khiareddine.bensalah@esprit.tn>
2024-12-08 07:33:46 -1200

    ajout h2DataBase pour les tests

--- Backend/src/test/java/com/formation/projet/ProjetFormationApplicationTests.java
+++ Backend/src/test/java/com/formation/projet/ProjetFormationApplicationTests.java
@@ -2,6 +2,9 @@ package com.formation.projet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class ProjetFormationApplicationTests {
