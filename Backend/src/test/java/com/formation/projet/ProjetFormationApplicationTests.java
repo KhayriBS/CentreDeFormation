de32006d6fb15caa0d112f52838ee6444ab4bd08
BEN SALAH Khaireddine <khiareddine.bensalah@esprit.tn>
2024-12-08 08:41:16 -1200

    ajout dependance h2DataBase

--- Backend/src/test/java/com/formation/projet/ProjetFormationApplicationTests.java
+++ Backend/src/test/java/com/formation/projet/ProjetFormationApplicationTests.java
@@ -1,12 +1,15 @@
 package com.formation.projet;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootTest
 class ProjetFormationApplicationTests {
 
