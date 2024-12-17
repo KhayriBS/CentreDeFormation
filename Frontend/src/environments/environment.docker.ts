174abac3d942776a450494dc9d80cce8c95da3d2
BEN SALAH Khaireddine <khiareddine.bensalah@esprit.tn>
2024-12-17 09:28:18 -1200

    17/12/2024 22:28

--- Frontend/src/environments/environment.docker.ts
+++ Frontend/src/environments/environment.docker.ts
@@ -1,15 +1,14 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.50.4:9090',
-    apiSessionUrl: 'http://192.168.50.4:9090/session',
-    apiParticipantUrl: 'http://192.168.50.4:9090/participant',
-    baseUrlAuth: 'http://192.168.50.4:9090/auth/',
-    apiServerUrlDomaine: 'http://192.168.50.4:9090/domaine',
-    apiServerUrlFormateur: 'http://192.168.50.4:9090/formateur',
-    apiServerUrlFormation: 'http://192.168.50.4:9090/formation',
-    apiServerUrlOrganisme: 'http://192.168.50.4:9090/organisme',
-    apiServerUrlParticipant: 'http://192.168.50.4:9090/participant',
-    apiServerUrlPays: 'http://192.168.50.4:9090/pays',
-    apiServerUrlProfil: 'http://192.168.50.4:9090/profil',
-  };
-  
+    apiUrl: 'http://backend:9090',
+    apiSessionUrl: 'http://backend:9090/session',
+    apiParticipantUrl: 'http://backend:9090/participant',
+    baseUrlAuth: 'http://backend:9090/auth/',
+    apiServerUrlDomaine: 'http://backend:9090/domaine',
+    apiServerUrlFormateur: 'http://backend:9090/formateur',
+    apiServerUrlFormation: 'http://backend:9090/formation',
+    apiServerUrlOrganisme: 'http://backend:9090/organisme',
+    apiServerUrlParticipant: 'http://backend:9090/participant',
+    apiServerUrlPays: 'http://backend:9090/pays',
+    apiServerUrlProfil: 'http://backend:9090/profil',
+};
