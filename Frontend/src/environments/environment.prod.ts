9748c07c35245da7813b8eb9bd082addd77f1685
BEN SALAH Khaireddine <khiareddine.bensalah@esprit.tn>
2024-12-23 20:05:00 -1200

    23/12/2024  update file environment.prod.ts

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 
   export const environment = {
     production: true,
-    apiUrl: 'http://backend:9095',
-    apiSessionUrl: 'http://backend:9095/session',
-    apiParticipantUrl: 'http://backend:9095/participant',
-    baseUrlAuth: 'http://backend:9095/auth/',
-    apiServerUrlDomaine: 'http://backend:9095/domaine',
-    apiServerUrlFormateur: 'http://backend:9095/formateur',
-    apiServerUrlFormation: 'http://backend:9095/formation',
-    apiServerUrlOrganisme: 'http://backend:9095/organisme',
-    apiServerUrlPays: 'http://backend:9095/pays',
-    apiServerUrlProfil: 'http://backend:9095/profil',
+    apiUrl: 'http://192.168.50.4:9095',
+    apiSessionUrl: 'http://192.168.50.4:9095/session',
+    apiParticipantUrl: 'http://192.168.50.4:9095/participant',
+    baseUrlAuth: 'http://192.168.50.4:9095/auth/',
+    apiServerUrlDomaine: 'http://192.168.50.4:9095/domaine',
+    apiServerUrlFormateur: 'http://192.168.50.4:9095/formateur',
+    apiServerUrlFormation: 'http://192.168.50.4:9095/formation',
+    apiServerUrlOrganisme: 'http://192.168.50.4:9095/organisme',
+    apiServerUrlPays: 'http://192.168.50.4:9095/pays',
+    apiServerUrlProfil: 'http://192.168.50.4:9095/profil',
   };
 
 
