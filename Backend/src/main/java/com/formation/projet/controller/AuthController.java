fca30f6f60e060f336eae45a300ceb4e7289ac40
BEN SALAH Khaireddine <khiareddine.bensalah@esprit.tn>
2024-12-24 16:13:17 -1200

    24/12/2024 05:13

--- Backend/src/main/java/com/formation/projet/controller/AuthController.java
+++ Backend/src/main/java/com/formation/projet/controller/AuthController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://192.168.50.4:4202") // Autoriser uniquement l'origine spécifiée
+@CrossOrigin(origins = "http://192.168.50.4:4202/login") // Autoriser uniquement l'origine spécifiée
 @RestController
 
 @RequestMapping("/auth")
