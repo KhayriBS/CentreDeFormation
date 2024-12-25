b23bb3cf7ea47c51d99f2cc6d2bd74cc089f4c42
BEN SALAH Khaireddine <khiareddine.bensalah@esprit.tn>
2024-12-24 14:17:27 -1200

    24/12/2024 environment.docker

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -4,16 +4,17 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.50.4:9095',
-  apiSessionUrl: 'http://192.168.50.4:9095/session',
-  apiParticipantUrl: 'http://192.168.50.4:9095/participant',
-  baseUrlAuth: 'http://192.168.50.4:9095/auth/',
-  apiServerUrlDomaine: 'http://192.168.50.4:9095/domaine',
-  apiServerUrlFormateur: 'http://192.168.50.4:9095/formateur',
-  apiServerUrlFormation: 'http://192.168.50.4:9095/formation',
-  apiServerUrlOrganisme: 'http://192.168.50.4:9095/organisme',
-  apiServerUrlPays: 'http://192.168.50.4:9095/pays',
-  apiServerUrlProfil: 'http://192.168.50.4:9095/profil',
+  apiUrl: 'http://localhost:9095',
+  apiSessionUrl: 'http://localhost:9095/session',
+  apiParticipantUrl: 'http://localhost:9095/participant',
+  baseUrlAuth : 'http://backend:9095/auth/',
+  apiServerUrlDomaine: 'http://localhost:9095/domaine',
+  apiServerUrlFormateur: 'http://localhost:9095/formateur',
+  apiServerUrlFormation: 'http://localhost:9095/formation',
+  apiServerUrlOrganisme: 'http://localhost:9095/organisme',
+  apiServerUrlParticipant: 'http://localhost:9095/participant',
+  apiServerUrlPays: 'http://localhost:9095/pays',
+  apiServerUrlProfil: 'http://localhost:9095/profil',
 
 
 
---
+++ Frontend/src/environments/environments.docker.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://192.168.50.4:9095',
+  apiSessionUrl: 'http://192.168.50.4:9095/session',
+  apiParticipantUrl: 'http://192.168.50.4:9095/participant',
+  baseUrlAuth: 'http://192.168.50.4:9095/auth/',
+  apiServerUrlDomaine: 'http://192.168.50.4:9095/domaine',
+  apiServerUrlFormateur: 'http://192.168.50.4:9095/formateur',
+  apiServerUrlFormation: 'http://192.168.50.4:9095/formation',
+  apiServerUrlOrganisme: 'http://192.168.50.4:9095/organisme',
+  apiServerUrlPays: 'http://192.168.50.4:9095/pays',
+  apiServerUrlProfil: 'http://192.168.50.4:9095/profil',
+};
